5dacf12bcb797f1c281b657b48449cc770ab6882
Alexei <achmelev2@Alexeis-Mac-mini.fritz.box>
2023-08-27 17:56:34 +0200

    Model - erste Schritte (2)

--- model.py
+++ model.py
@@ -18,13 +18,13 @@ from environment import log, get_float_config_value, get_int_config_value, get_b
 
 
 class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-    dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    block_size: int
+    vocab_size: int
+    n_layer: int
+    n_head: int
+    n_embd: int
+    dropout: float
+    bias: bool
 
 _config = None
 
