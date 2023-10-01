257450f8db4e624dadfb656e4751891f1386335e
Alexei <achmelev2@Alexeis-Mac-mini.fritz.box>
2023-10-01 14:54:27 +0200

    tokentreezer optimizing

--- tokentreezer.py
+++ tokentreezer.py
@@ -18,26 +18,29 @@ class TokenTreezer:
     def write(self, depth):
         assert depth>0, 'illegal depth '+str(depth)
         tree = TokenTree(workDir+"tokentree.bin",'w')
+        initialTreeDepth = tree.depth
         if (tree.depth == 0):
             tree.initFirstLevel(get_int_config_value('vocab_size'))
             log.info("Created and initialized a tree")
         else:
             log.info("Starting with existing tree of depth "+ str(tree.depth)+", and size = "+str(tree.size))
-        if (depth <= tree.depth):
+
+        if (depth <= initialTreeDepth):
             log.warn('Tree already filled up to depth '+ str(tree.depth)+ ', quitting.')
+            tree.close()
+            return
         else:
-            startDepth = tree.depth+1
+            startDepth = initialTreeDepth+1
             stopDepth = depth
         
         for idx in range(len(self.train_data)):
+            if (idx%1000 == 0):
+                log.debug("Train data index is "+str(idx)+", tree size "+str(tree.size))
             maxl = min(stopDepth, len(self.train_data)-idx)
             sequence = self.train_data[idx:idx+maxl]
-            for idx2, _ in enumerate(sequence):
-                subsequence = sequence[0:idx2+1]
-                if (len(subsequence) >= startDepth):
-                    tree.insertOrUpdateToken(self.int_list_from_numpy(subsequence))
-                    if (tree.size%1000 == 0):
-                        log.debug('Got '+str(tree.size)+", nodes, current index in train data is = "+str(idx))
+            for idx2 in range(startDepth,len(sequence)+1):
+                subsequence = sequence[0:idx2]
+                tree.insertOrUpdateToken(self.int_list_from_numpy(subsequence))
         
         log.info('Done. Got a tree with size = '+str(tree.size))
         tree.close()
