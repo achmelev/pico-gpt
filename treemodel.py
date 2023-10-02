7f6dd0ff388ee74742a37f149c36c5ac7e5a84b4
Alexei Chmelev <alexei_chmelev@gmx.de>
2023-10-02 22:46:21 +0200

    made tree a global variable (caching preparation)

--- treemodel.py
+++ treemodel.py
@@ -6,43 +6,52 @@ from numpy import zeros, float32
 from environment import log, get_int_config_value, workDir
 from tokentree import TokenTree
 from timers import create_timer, start, stop 
+from functools import lru_cache
+
+tree = None
 
 class TokenTreeModel(nn.Module):
 
     def __init__(self):
         super().__init__()
+        global tree
+        assert tree == None,'This model is a singleton!'
         self.vocab_size = get_int_config_value('vocab_size')
         self.block_size = get_int_config_value('block_size')
-        self.tree = TokenTree(workDir+'tokentree.bin','r')
-        assert self.vocab_size==self.tree.vocab_size,'Wrong vocab size '+str(self.vocab_size)+"!="+str(self.tree.vocab_size)
-        log.info('Initialized tree model vocab_size = '+str(self.vocab_size)+", tree size "+str(self.tree.size)+", tree depth = "+str(self.tree.depth))
-        self.linear = nn.Linear(self.tree.depth,1)
+        tree = TokenTree(workDir+'tokentree.bin','r')
+        assert self.vocab_size==tree.vocab_size,'Wrong vocab size '+str(self.vocab_size)+"!="+str(tree.vocab_size)
+        log.info('Initialized tree model vocab_size = '+str(self.vocab_size)+", tree size "+str(tree.size)+", tree depth = "+str(tree.depth))
+        self.linear = nn.Linear(tree.depth,1)
         torch.nn.init.normal_(self.linear.weight, mean=0.0, std=0.02)
         self.np_array = None
+        self.tree = tree
 
     def __del__(self):
-        self.tree.close()
+        global tree
+        tree.close()
+        tree = None
     
     def create_ml_input(self, idx, inference=False):
+        global tree
         b, t = idx.size()
         if (self.np_array == None):
             if (inference):
-                self.nparray = zeros((b,1,self.tree.depth, self.vocab_size),dtype=float32)
+                self.nparray = zeros((b,1,tree.depth, self.vocab_size),dtype=float32)
                 t_start = t-1
             else:
-                self.nparray = zeros((b,t,self.tree.depth, self.vocab_size),dtype=float32)
+                self.nparray = zeros((b,t,tree.depth, self.vocab_size),dtype=float32)
                 t_start = 0
 
         zero_value = 0.0
-        firstLevel = self.tree.getNodesChildren([])
+        firstLevel = tree.getNodesChildren([])
         for b_idx in range(b):
             for t_idx in range(t_start,t):
-                for tree_idx in range(self.tree.depth):
+                for tree_idx in range(tree.depth):
                     start_idx = t_idx-tree_idx
                     if (start_idx>=0):
                         sequence = idx[b_idx,start_idx:t_idx].tolist()
                         if (len(sequence)>0):
-                            children = self.tree.getNodesChildren(sequence)
+                            children = tree.getNodesChildren(sequence)
                         else:
                             children = firstLevel
 
